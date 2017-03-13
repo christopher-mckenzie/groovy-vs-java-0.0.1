42cb4d24e1cbb8abdb012a77c7458508a3d7fd96
christopher-mckenzie <cmckenzie59@gmail.com>
2017-03-13 16:46:04 -0500

    xml

--- src/test/groovy/com/tutorial/XmlSpec.groovy
+++ src/test/groovy/com/tutorial/XmlSpec.groovy
@@ -8,21 +8,47 @@ import spock.lang.Specification
  */
 class XmlSpec extends Specification{
 
-    def "xml slurper -> read in xml"(){
+    def "xml parser -> read in xml"(){
         given:
             List<DriverGroovy> drivers = new ArrayList<>()
         when:
-        def xml = new XmlParser().parse("sample.xml") //parse xml -> turns xml into DOM
-        //def driversXml = xml.driver
-        xml.driver.each { driver ->
-            def name = driver.name
-            def number = Integer.parseInt driver.number.text().trim()
-            def averageFinish = Double.parseDouble driver.average_finish.text().trim()
-            def careerWins = Integer.parseInt driver.career_wins.text().trim()
-            drivers << new DriverGroovy(name: name, number: number, averageFinish: averageFinish, careerWins: careerWins)
-        }
+            def xml = new XmlParser().parse("sample.xml") //parse xml -> turns xml into DOM
+            xml.driver.each { driver ->
+                def name = driver.name
+                def number = Integer.parseInt driver.number.text().trim()
+                def averageFinish = Double.parseDouble driver.average_finish.text().trim()
+                def careerWins = Integer.parseInt driver.career_wins.text().trim()
+                drivers << new DriverGroovy(
+                        name: name,
+                        number: number,
+                        averageFinish: averageFinish,
+                        careerWins: careerWins
+                )
+            }
 
         then:
             drivers.size() == 2
+    }
+
+    def "xml parser with attributes -> read in xml"(){
+
+    }
+    def "xml builder -> create xml file"(){
+//                MarkupBuilder builder = new MarkupBuilder()
+//        builder.games { //games is not a method but rather here it is a node
+//            results.each { g ->
+//                game ( //creates a game node with the following attributes
+//                        outcome:"$g.away $g.aScore, $g.home $g.hScore",
+//                        lat:g.stadium.latitude,
+//                        lng:g.stadium.longitude
+//                )
+//            }
+//        }
+        /** sample output
+         * <games>
+         *     <game outcome='Boston Red Sox 4, Colorado Rockies 3'
+         *       lat='39.7564956' lng='-104.9940163'/>
+         * </games>
+         */
     }
 }
