23489199b83a07acd5b850049e568c28cb4741cc
christopher-mckenzie <cmckenzie59@gmail.com>
2017-03-14 16:09:07 -0500

    xml

--- src/test/groovy/com/tutorial/XmlSpec.groovy
+++ src/test/groovy/com/tutorial/XmlSpec.groovy
@@ -1,6 +1,7 @@
 package com.tutorial
 
 import com.tutorial.bean.DriverGroovy
+import groovy.xml.MarkupBuilder
 import spock.lang.Specification
 
 /**
@@ -12,7 +13,7 @@ class XmlSpec extends Specification{
         given:
             List<DriverGroovy> drivers = new ArrayList<>()
         when:
-            def xml = new XmlParser().parse("sample.xml") //parse xml -> turns xml into DOM
+            def xml = new XmlParser().parse("sample_reader.xml") //parse xml -> turns xml into DOM
             xml.driver.each { driver ->
                 def name = driver.name
                 def number = Integer.parseInt driver.number.text().trim()
@@ -27,28 +28,73 @@ class XmlSpec extends Specification{
             }
 
         then:
+            println drivers
             drivers.size() == 2
     }
 
-    def "xml parser with attributes -> read in xml"(){
+    def "xml parser with annotations -> read in xml"(){
+        given:
+            List<DriverGroovy> drivers = new ArrayList<>()
+        when:
+            def xml = new XmlParser().parse("sample_reader_annotation.xml") //parse xml -> turns xml into DOM
+            xml.driver.each { driver ->
+                drivers << new DriverGroovy(
+                        name: driver.@name,
+                        number: Integer.parseInt(driver.@number),
+                        averageFinish: Double.parseDouble(driver.@average_finish),
+                        careerWins: Integer.parseInt(driver.@career_wins)
+                )
+            }
 
+        then:
+            println drivers
+            drivers.size() == 2
     }
+
     def "xml builder -> create xml file"(){
-//                MarkupBuilder builder = new MarkupBuilder()
-//        builder.games { //games is not a method but rather here it is a node
-//            results.each { g ->
-//                game ( //creates a game node with the following attributes
-//                        outcome:"$g.away $g.aScore, $g.home $g.hScore",
-//                        lat:g.stadium.latitude,
-//                        lng:g.stadium.longitude
-//                )
-//            }
-//        }
-        /** sample output
-         * <games>
-         *     <game outcome='Boston Red Sox 4, Colorado Rockies 3'
-         *       lat='39.7564956' lng='-104.9940163'/>
-         * </games>
-         */
+        given:
+            List<DriverGroovy> driversGroovy = new ArrayList<>()
+            driversGroovy << new DriverGroovy(name: 'Dale Enhardt Jr', number: 88, averageFinish: 13.2, careerWins: 98)
+            driversGroovy << new DriverGroovy(name: 'Dale Enhardt Jr', number: 88, averageFinish: 13.2, careerWins: 98)
+        when:
+            FileWriter fileWriter = new FileWriter("sample_writer.xml")
+            MarkupBuilder builder = new MarkupBuilder(fileWriter)
+            builder.drivers { //games is not a method but rather here it is a node
+                driversGroovy.each { dg ->
+                    driver { //creates a game node with the following attributes
+                        name("$dg.name")
+                        number(dg.number)
+                        average_finish(dg.averageFinish)
+                        career_wins(dg.careerWins)
+                    }
+                }
+           }
+            fileWriter.close()
+        then:
+            driversGroovy.size() == 2
+    }
+
+
+    def "xml builder with attributes -> create xml file"(){
+        given:
+            List<DriverGroovy> driversGroovy = new ArrayList<>()
+            driversGroovy << new DriverGroovy(name: 'Dale Enhardt Jr', number: 88, averageFinish: 13.2, careerWins: 98)
+            driversGroovy << new DriverGroovy(name: 'Dale Enhardt Jr', number: 88, averageFinish: 13.2, careerWins: 98)
+        when:
+            FileWriter fileWriter = new FileWriter("sample_writer_annotation.xml")
+            MarkupBuilder builder = new MarkupBuilder(fileWriter)
+            builder.drivers { //games is not a method but rather here it is a node
+                driversGroovy.each { dg ->
+                    driver ( //creates a game node with the following attributes
+                        name: "$dg.name",
+                        number: dg.number,
+                        average_finish: dg.averageFinish,
+                        career_wins: dg.careerWins
+                    )
+                }
+            }
+            fileWriter.close()
+        then:
+            driversGroovy.size() == 2
     }
 }
